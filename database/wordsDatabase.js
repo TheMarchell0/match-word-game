60f4ece32a1259204bbbc07f23222727ed9577a1
TheMarchell0 <TheMarchell0@yandex.ru>
2024-03-22 21:46:12 +0600

    initial

---
+++ database/wordsDatabase.js
@@ -0,0 +1,10 @@
+export const wordsDatabase = {
+    'lvl_1' : {
+        'mainWord': 'mainWord1',
+        'searchWords': ['searchWord1', 'searchWords2']
+    },
+    'lvl_2' : {
+        'mainWord': 'mainWord2',
+        'searchWords': ['searchWord1', 'searchWords2']
+    }
+}
